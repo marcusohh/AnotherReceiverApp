29f36a1385f8e489e11b93240ba3164b89e4b290
marcusohh <16020089@myrp.edu.sg>
2018-08-13 14:53:00 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/anotherreceiverapp/AnotherBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package sg.edu.rp.c346.anotherreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by 16020089 on 13/8/2018.
+ */
+
+public class AnotherBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Received in AnotherBroadcastReceiver",Toast.LENGTH_LONG).show();;
+
+    }
+}
